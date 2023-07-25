9e3fe0add0f657acde2f4ac3e853ac96059bbde1
Justin Yamamoto <99459474+JustinYSJSU@users.noreply.github.com>
2023-07-25 14:17:55 -0700

    added support for deadlock

--- botApp/bot.py
+++ botApp/bot.py
@@ -41,6 +41,7 @@ agent_uuid_dict = {
  "brimstone": "9f0d8ba9-4140-b941-57d3-a7ad57c6b417", 
  "chamber": "22697a3d-45bf-8dd7-4fec-84a9e28c69d7", 
  "cypher": "117ed9e3-49f3-6512-3ccf-0cada7e3823b", 
+ "deadlock": "cc8b64c8-4b25-4ff9-6e7f-37b4da43d235",
  "fade": "dade69b4-4f5a-8528-247b-219e5a1facd6", 
  "gekko": "e370fa57-4757-3604-3648-499e1f642d3f",
  "harbor": "95b78ed7-4637-86d9-7e41-71ba8c293152",
